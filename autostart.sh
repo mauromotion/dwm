64b7ea9356ee8f70fdceed47780b7a05763f6282
mauromotion <hello@mauromotion.com>
2024-06-17 19:19:10 +0100

    added autorstart.sh

--- autostart.sh
+++ autostart.sh
@@ -10,3 +10,8 @@ nm-applet &
 nextcloud --background &
 openrgb --startminimized &
 xset b 100 &
+dispwin -d1 "$HOME/.config/i3/color-profiles/icc/Dell_S2721DGF.icm"
+
+sleep 1
+
+dispwin -d2 "$HOME/.config/i3/color-profiles/icc/Dell_U2713HM.icm"
